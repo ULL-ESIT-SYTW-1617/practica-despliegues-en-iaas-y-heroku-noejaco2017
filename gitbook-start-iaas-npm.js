193d6ea07ac1678b2199479cac3667f54d46d0ee
alu0100622492 <alu0100622492@ull.edu.es>
2017-01-04 01:59:41 +0000

    Reconstruyendo plugin a una version npm

---
+++ gitbook-start-iaas-npm.js
@@ -0,0 +1,8 @@
+
+
+
+
+var contenido = `gulp.task("push-heroku" ,function(){
+            var heroku = require("gitbook-start-heroku-localstrategy-noejaco17");
+            heroku.deploy();
+     });`;
